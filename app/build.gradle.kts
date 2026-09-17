89923420488a3bbfcb2096cd795619bf887fd140
anilkumar543308-hue <anilkumar543308@gmail.com>
2026-09-17 18:40:33 +0545

    Fix formatting in debug block of build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,9 +45,9 @@ android {
       isMinifyEnabled = false
       proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
       signingConfig = signingConfigs.getByName("release")
-    }
-    debug { 
-  }
+  debug {
+        }
+  
   compileOptions {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
